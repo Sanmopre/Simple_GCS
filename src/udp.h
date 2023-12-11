a88ca1b99d0476e800d66f13712a74ebe3887b7f
Santiago Moliner <40120264+Sanmopre@users.noreply.github.com>
2023-12-11 21:16:42 +0100

    Fixed udp server

--- src/udp.h
+++ src/udp.h
@@ -5,16 +5,35 @@
 #include <iostream>
 #include <string>
 
-
 struct UDPServer
 {   
-    void Connect(std::string ip, int port);
-    void Send(std::string message);
+    UDPServer() : resolver(io_service), socket(io_service) {}
+
     boost::asio::io_service io_service;
     boost::asio::ip::udp::resolver resolver;
     boost::asio::ip::udp::endpoint receiver_endpoint;
     boost::asio::ip::udp::socket socket;
-};
 
+    void Connect(std::string ip, int port)
+    {
+        try {
+            receiver_endpoint = *resolver.resolve({boost::asio::ip::udp::v4(), ip, std::to_string(port)}).begin();
+            socket.open(boost::asio::ip::udp::v4());
+        }
+        catch (std::exception& e) {
+            std::cerr << e.what() << std::endl;
+        }
+    }
+
+    void Send(std::string message)
+    {
+        try {
+            socket.send_to(boost::asio::buffer(message), receiver_endpoint);
+        }
+        catch (std::exception& e) {
+            std::cerr << e.what() << std::endl;
+        }
+    }
+};
 
 #endif // UDP_H
