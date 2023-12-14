ab63c04aa21d93d12220c98c171225a46f395969
Santiago Moliner <40120264+Sanmopre@users.noreply.github.com>
2023-12-14 22:07:07 +0100

    Rotation controls and Map camvas

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,89 @@
 
 #define GL_SILENCE_DEPRECATION
 
+void embraceTheDarkness()
+{
+  ImVec4* colors = ImGui::GetStyle().Colors;
+  colors[ImGuiCol_Text]                   = ImVec4(1.00f, 1.00f, 1.00f, 1.00f);
+  colors[ImGuiCol_TextDisabled]           = ImVec4(0.50f, 0.50f, 0.50f, 1.00f);
+  colors[ImGuiCol_WindowBg]               = ImVec4(0.10f, 0.10f, 0.10f, 1.00f);
+  colors[ImGuiCol_ChildBg]                = ImVec4(0.00f, 0.00f, 0.00f, 0.00f);
+  colors[ImGuiCol_PopupBg]                = ImVec4(0.19f, 0.19f, 0.19f, 0.92f);
+  colors[ImGuiCol_Border]                 = ImVec4(0.19f, 0.19f, 0.19f, 0.29f);
+  colors[ImGuiCol_BorderShadow]           = ImVec4(0.00f, 0.00f, 0.00f, 0.24f);
+  colors[ImGuiCol_FrameBg]                = ImVec4(0.05f, 0.05f, 0.05f, 0.54f);
+  colors[ImGuiCol_FrameBgHovered]         = ImVec4(0.19f, 0.19f, 0.19f, 0.54f);
+  colors[ImGuiCol_FrameBgActive]          = ImVec4(0.20f, 0.22f, 0.23f, 1.00f);
+  colors[ImGuiCol_TitleBg]                = ImVec4(0.00f, 0.00f, 0.00f, 1.00f);
+  colors[ImGuiCol_TitleBgActive]          = ImVec4(0.06f, 0.06f, 0.06f, 1.00f);
+  colors[ImGuiCol_TitleBgCollapsed]       = ImVec4(0.00f, 0.00f, 0.00f, 1.00f);
+  colors[ImGuiCol_MenuBarBg]              = ImVec4(0.14f, 0.14f, 0.14f, 1.00f);
+  colors[ImGuiCol_ScrollbarBg]            = ImVec4(0.05f, 0.05f, 0.05f, 0.54f);
+  colors[ImGuiCol_ScrollbarGrab]          = ImVec4(0.34f, 0.34f, 0.34f, 0.54f);
+  colors[ImGuiCol_ScrollbarGrabHovered]   = ImVec4(0.40f, 0.40f, 0.40f, 0.54f);
+  colors[ImGuiCol_ScrollbarGrabActive]    = ImVec4(0.56f, 0.56f, 0.56f, 0.54f);
+  colors[ImGuiCol_CheckMark]              = ImVec4(0.33f, 0.67f, 0.86f, 1.00f);
+  colors[ImGuiCol_SliderGrab]             = ImVec4(0.34f, 0.34f, 0.34f, 0.54f);
+  colors[ImGuiCol_SliderGrabActive]       = ImVec4(0.56f, 0.56f, 0.56f, 0.54f);
+  colors[ImGuiCol_Button]                 = ImVec4(0.05f, 0.05f, 0.05f, 0.54f);
+  colors[ImGuiCol_ButtonHovered]          = ImVec4(0.19f, 0.19f, 0.19f, 0.54f);
+  colors[ImGuiCol_ButtonActive]           = ImVec4(0.20f, 0.22f, 0.23f, 1.00f);
+  colors[ImGuiCol_Header]                 = ImVec4(0.00f, 0.00f, 0.00f, 0.52f);
+  colors[ImGuiCol_HeaderHovered]          = ImVec4(0.00f, 0.00f, 0.00f, 0.36f);
+  colors[ImGuiCol_HeaderActive]           = ImVec4(0.20f, 0.22f, 0.23f, 0.33f);
+  colors[ImGuiCol_Separator]              = ImVec4(0.28f, 0.28f, 0.28f, 0.29f);
+  colors[ImGuiCol_SeparatorHovered]       = ImVec4(0.44f, 0.44f, 0.44f, 0.29f);
+  colors[ImGuiCol_SeparatorActive]        = ImVec4(0.40f, 0.44f, 0.47f, 1.00f);
+  colors[ImGuiCol_ResizeGrip]             = ImVec4(0.28f, 0.28f, 0.28f, 0.29f);
+  colors[ImGuiCol_ResizeGripHovered]      = ImVec4(0.44f, 0.44f, 0.44f, 0.29f);
+  colors[ImGuiCol_ResizeGripActive]       = ImVec4(0.40f, 0.44f, 0.47f, 1.00f);
+  colors[ImGuiCol_Tab]                    = ImVec4(0.00f, 0.00f, 0.00f, 0.52f);
+  colors[ImGuiCol_TabHovered]             = ImVec4(0.14f, 0.14f, 0.14f, 1.00f);
+  colors[ImGuiCol_TabActive]              = ImVec4(0.20f, 0.20f, 0.20f, 0.36f);
+  colors[ImGuiCol_TabUnfocused]           = ImVec4(0.00f, 0.00f, 0.00f, 0.52f);
+  colors[ImGuiCol_TabUnfocusedActive]     = ImVec4(0.14f, 0.14f, 0.14f, 1.00f);
+  colors[ImGuiCol_DockingPreview]         = ImVec4(0.33f, 0.67f, 0.86f, 1.00f);
+  colors[ImGuiCol_DockingEmptyBg]         = ImVec4(1.00f, 0.00f, 0.00f, 1.00f);
+  colors[ImGuiCol_PlotLines]              = ImVec4(1.00f, 0.00f, 0.00f, 1.00f);
+  colors[ImGuiCol_PlotLinesHovered]       = ImVec4(1.00f, 0.00f, 0.00f, 1.00f);
+  colors[ImGuiCol_PlotHistogram]          = ImVec4(1.00f, 0.00f, 0.00f, 1.00f);
+  colors[ImGuiCol_PlotHistogramHovered]   = ImVec4(1.00f, 0.00f, 0.00f, 1.00f);
+  colors[ImGuiCol_TableHeaderBg]          = ImVec4(0.00f, 0.00f, 0.00f, 0.52f);
+  colors[ImGuiCol_TableBorderStrong]      = ImVec4(0.00f, 0.00f, 0.00f, 0.52f);
+  colors[ImGuiCol_TableBorderLight]       = ImVec4(0.28f, 0.28f, 0.28f, 0.29f);
+  colors[ImGuiCol_TableRowBg]             = ImVec4(0.00f, 0.00f, 0.00f, 0.00f);
+  colors[ImGuiCol_TableRowBgAlt]          = ImVec4(1.00f, 1.00f, 1.00f, 0.06f);
+  colors[ImGuiCol_TextSelectedBg]         = ImVec4(0.20f, 0.22f, 0.23f, 1.00f);
+  colors[ImGuiCol_DragDropTarget]         = ImVec4(0.33f, 0.67f, 0.86f, 1.00f);
+  colors[ImGuiCol_NavHighlight]           = ImVec4(1.00f, 0.00f, 0.00f, 1.00f);
+  colors[ImGuiCol_NavWindowingHighlight]  = ImVec4(1.00f, 0.00f, 0.00f, 0.70f);
+  colors[ImGuiCol_NavWindowingDimBg]      = ImVec4(1.00f, 0.00f, 0.00f, 0.20f);
+  colors[ImGuiCol_ModalWindowDimBg]       = ImVec4(1.00f, 0.00f, 0.00f, 0.35f);
+
+  ImGuiStyle& style = ImGui::GetStyle();
+  style.WindowPadding                     = ImVec2(8.00f, 8.00f);
+  style.FramePadding                      = ImVec2(5.00f, 2.00f);
+  style.CellPadding                       = ImVec2(6.00f, 6.00f);
+  style.ItemSpacing                       = ImVec2(6.00f, 6.00f);
+  style.ItemInnerSpacing                  = ImVec2(6.00f, 6.00f);
+  style.TouchExtraPadding                 = ImVec2(0.00f, 0.00f);
+  style.IndentSpacing                     = 25;
+  style.ScrollbarSize                     = 15;
+  style.GrabMinSize                       = 10;
+  style.WindowBorderSize                  = 1;
+  style.ChildBorderSize                   = 1;
+  style.PopupBorderSize                   = 1;
+  style.FrameBorderSize                   = 1;
+  style.TabBorderSize                     = 1;
+  style.WindowRounding                    = 7;
+  style.ChildRounding                     = 4;
+  style.FrameRounding                     = 3;
+  style.PopupRounding                     = 4;
+  style.ScrollbarRounding                 = 9;
+  style.GrabRounding                      = 3;
+  style.LogSliderDeadzone                 = 4;
+  style.TabRounding                       = 4;
+}
 
 int main(int, char**)
 {
@@ -61,10 +144,8 @@ int main(int, char**)
     ImGui_ImplOpenGL3_Init(glsl_version);
 
     io.Fonts->AddFontFromFileTTF("../assets/fonts/CubicCoreMono.ttf", 15.0f);
-
+    embraceTheDarkness();
     // Our state
-    bool show_demo_window = true;
-    bool show_another_window = false;
     ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
 
@@ -77,6 +158,10 @@ int main(int, char**)
     int engine_1_rpm = 0;
     int engine_2_rpm = 0;
 
+    float pitch = 0.0f;
+    float roll = 0.0f;
+    float yaw = 0.0f;
+
     enum class Flaps { LANDING, TAKEOFF, CRUISE };
     static Flaps flaps = Flaps::LANDING;
     bool both_engines_control = false;
@@ -86,9 +171,9 @@ int main(int, char**)
     bool ret = LoadTextureFromFile("../assets/images/AirbusLogo.png", &airbus_image);
     IM_ASSERT(ret);
 
-    Image map_image;
-    ret = LoadTextureFromFile("../assets/images/map.jpeg", &map_image);
-    IM_ASSERT(ret);
+    //Image map_image;
+    //ret = LoadTextureFromFile("../assets/images/map.jpeg", &map_image);
+    //IM_ASSERT(ret);
 
     UDPServer udp_server;
     udp_server.Connect(config_parser->ip, config_parser->port);
@@ -98,7 +183,6 @@ int main(int, char**)
     {
 
         glfwPollEvents();
-        //SendUDPMessage(std::to_string(trhust_engine_1), config_parser->ip, config_parser->port);
 
         std::string message = std::string(std::to_string(trhust_engine_1) + "," + std::to_string(trhust_engine_2) + "," + std::to_string(static_cast<int>(flaps)));
         udp_server.Send(message);
@@ -107,9 +191,9 @@ int main(int, char**)
         ImGui_ImplOpenGL3_NewFrame();
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
-
+        ImGui::DockSpaceOverViewport(ImGui::GetMainViewport(), ImGuiDockNodeFlags_NoUndocking| ImGuiDockNodeFlags_NoResize);
         
-        ImGui::ShowDemoWindow(&show_demo_window);
+        //ImGui::ShowDemoWindow(&show_demo_window);
 
         ImGui::BeginMainMenuBar();  
         ImGui::Text("GCS");
@@ -125,6 +209,7 @@ int main(int, char**)
         ImGui::Checkbox("Both engines control", &both_engines_control);
         if (both_engines_control) 
         {
+            ImGui::Text("Engines");
             ImGui::SliderFloat("Thrust Power", &trhust_engine_1, 0.0f, 100.0f);
             trhust_engine_2 = trhust_engine_1;
         }
@@ -143,28 +228,34 @@ int main(int, char**)
 
         ImGui::Text("Engine 1 Temperature: ");
         ImGui::SameLine();
-        ImGui::TextColored(ImVec4(1, 0, 0, 1), temperature_1_string.c_str());
+        ImGui::TextColored(ImVec4(1, 0, 0, 1), "%s", temperature_1_string.c_str());
 
         ImGui::Text("Engine 2 Temperature: ");
         ImGui::SameLine();
-        ImGui::TextColored(ImVec4(1, 0, 0, 1), temperature_2_string.c_str());
+        ImGui::TextColored(ImVec4(1, 0, 0, 1), "%s", temperature_2_string.c_str());
 
         std::string rpm_1_string = std::to_string(engine_1_rpm);
         std::string rpm_2_string = std::to_string(engine_2_rpm);
 
         ImGui::Text("Engine 1 RPM: ");
         ImGui::SameLine();
-        ImGui::TextColored(ImVec4(1, 0, 1, 1), rpm_1_string.c_str());
+        ImGui::TextColored(ImVec4(1, 0, 1, 1), "%s", rpm_1_string.c_str());
 
         ImGui::Text("Engine 2 RPM: ");
         ImGui::SameLine();
-        ImGui::TextColored(ImVec4(1, 0, 1, 1), rpm_2_string.c_str());
+        ImGui::TextColored(ImVec4(1, 0, 1, 1), "%s", rpm_2_string.c_str());
         ImGui::End();
 
         ImGui::Begin("LOGO", nullptr, IMGUI_WINDOW_FLAGS);
         ImGui::Image((void*)(intptr_t)airbus_image.texture, ImVec2(airbus_image.width / 10, airbus_image.height/ 10));
         ImGui::End();
 
+
+        ImGui::Begin("STABILIZER CONTROL", nullptr, IMGUI_WINDOW_FLAGS);
+        ImGui::SliderFloat("Pitch", &pitch, -90.0f, 90.0f);
+        ImGui::SliderFloat("Roll", &roll, -90.0f, 90.0f);
+        ImGui::SliderFloat("Yaw", &yaw, -90.0f, 90.0f);
+        ImGui::End();
 
 
         //Flaps control
@@ -175,18 +266,91 @@ int main(int, char**)
         ImGui::End();
 
 
-        ImGui::Begin("MAP", nullptr, IMGUI_WINDOW_FLAGS);
-        ImGui::Image((void*)(intptr_t)map_image.texture, ImVec2(map_image.width / 4, map_image.height / 4));
-        if (ImGui::IsItemHovered())
-        {
-            ImVec2 mouse_pos = ImGui::GetMousePos();
-            ImVec2 item_pos = ImGui::GetItemRectMin();
-            ImVec2 local_pos = ImVec2(mouse_pos.x - item_pos.x, mouse_pos.y - item_pos.y);
-            ImGui::BeginTooltip();
-            ImGui::Text("Pixel position: (%.1f,%.1f)", local_pos.x, local_pos.y);
-            ImGui::EndTooltip();
-        }
-        ImGui::End();
+        //ImGui::Begin("MAP", nullptr, IMGUI_WINDOW_FLAGS);
+        //ImGui::Image((void*)(intptr_t)map_image.texture, ImVec2(map_image.width / 4, map_image.height / 4));
+        //ImGui::End();
+
+
+            //Map
+            ImGui::Begin("MAP", nullptr, IMGUI_WINDOW_FLAGS);
+            static ImVector<ImVec2> points;
+            static ImVec2 scrolling(0.0f, 0.0f);
+            static bool opt_enable_context_menu = false;
+            const float GRID_STEP = 32.0f;
+
+            ImVec2 canvas_p0 = ImGui::GetCursorScreenPos();      // ImDrawList API uses screen coordinates!
+            ImVec2 canvas_sz = ImGui::GetContentRegionAvail();   // Resize canvas to what's available
+            if (canvas_sz.x < 50.0f) canvas_sz.x = 50.0f;
+            if (canvas_sz.y < 50.0f) canvas_sz.y = 50.0f;
+            ImVec2 canvas_p1 = ImVec2(canvas_p0.x + canvas_sz.x, canvas_p0.y + canvas_sz.y);
+
+            // Draw border and background color
+            ImGuiIO& io = ImGui::GetIO();
+            ImDrawList* draw_list = ImGui::GetWindowDrawList();
+            draw_list->AddRectFilled(canvas_p0, canvas_p1, IM_COL32(50, 50, 50, 255));
+            draw_list->AddRect(canvas_p0, canvas_p1, IM_COL32(255, 255, 255, 255));
+
+            // This will catch our interactions
+            ImGui::InvisibleButton("canvas", canvas_sz, ImGuiButtonFlags_MouseButtonLeft | ImGuiButtonFlags_MouseButtonRight);
+            const bool is_hovered = ImGui::IsItemHovered(); // Hovered
+            const bool is_active = ImGui::IsItemActive();   // Held
+            const ImVec2 origin(canvas_p0.x + scrolling.x, canvas_p0.y + scrolling.y); // Lock scrolled origin
+            const ImVec2 mouse_pos_in_canvas(io.MousePos.x - origin.x, io.MousePos.y - origin.y);
+
+            //testing drawing
+            /*
+            points.push_back({0.0f, 0.0f});
+            points.push_back({100.0f, 100.0f});
+
+            points.push_back({ 100.0f, 100.0f });
+            points.push_back({ 200.0f, 100.0f });
+
+            points.push_back({ 200.0f, 100.0f });
+            points.push_back({ 200.0f, 200.0f });
+
+            points.push_back({ 200.0f, 200.0f });
+            points.push_back({ 100.0f, 200.0f });
+            */
+
+
+            if(is_hovered)
+            {
+                ImGui::BeginTooltip();
+                ImGui::Text("Latitute: %f", mouse_pos_in_canvas.x);
+                ImGui::Text("Longitute: %f", mouse_pos_in_canvas.y);
+                ImGui::EndTooltip();
+            }
+
+            const float mouse_threshold_for_pan = opt_enable_context_menu ? -1.0f : 0.0f;
+            if (is_active && ImGui::IsMouseDragging(ImGuiMouseButton_Right, mouse_threshold_for_pan))
+            {
+                scrolling.x += io.MouseDelta.x;
+                scrolling.y += io.MouseDelta.y;
+            }
+
+            // Draw grid + all lines in the canvas
+            draw_list->PushClipRect(canvas_p0, canvas_p1, true);
+
+            for (float x = fmodf(scrolling.x, GRID_STEP); x < canvas_sz.x; x += GRID_STEP)
+                draw_list->AddLine(ImVec2(canvas_p0.x + x, canvas_p0.y), ImVec2(canvas_p0.x + x, canvas_p1.y), IM_COL32(200, 200, 200, 40));
+            for (float y = fmodf(scrolling.y, GRID_STEP); y < canvas_sz.y; y += GRID_STEP)
+                draw_list->AddLine(ImVec2(canvas_p0.x, canvas_p0.y + y), ImVec2(canvas_p1.x, canvas_p0.y + y), IM_COL32(200, 200, 200, 40));
+            
+            for (int n = 0; n < points.Size; n += 2)
+                draw_list->AddLine(ImVec2(origin.x + points[n].x, origin.y + points[n].y), ImVec2(origin.x + points[n + 1].x, origin.y + points[n + 1].y), IM_COL32(255, 255, 0, 255), 2.0f);
+            draw_list->PopClipRect();
+            ImGui::End();
+
+            ImGui::Begin("DRONE POSITION", nullptr, IMGUI_WINDOW_FLAGS);
+            ImGui::Text("Latitute: %f", 5.94);
+            ImGui::SameLine();
+            ImGui::Text("Speed: %f", 370.0f);
+            ImGui::Text("Longitute: %f", 45.04f);
+            ImGui::SameLine();
+            ImGui::Text("Vertical Speed: %f", 10.0f);
+            ImGui::Text("Altitude: %f", 3200.0f);
+            ImGui::End();
+    
 
         // Rendering
         ImGui::Render();
