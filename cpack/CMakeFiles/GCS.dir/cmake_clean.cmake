7c4b81acef9ea06336a5bb6ceb6477e9188de7fc
Santiago Moliner <40120264+Sanmopre@users.noreply.github.com>
2024-03-04 21:50:45 +0100

    ImGui subfolder

---
+++ cpack/CMakeFiles/GCS.dir/cmake_clean.cmake
@@ -0,0 +1,33 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GCS.dir/drone_data.pb.cc.o"
+  "CMakeFiles/GCS.dir/drone_data.pb.cc.o.d"
+  "CMakeFiles/GCS.dir/gcs_data.pb.cc.o"
+  "CMakeFiles/GCS.dir/gcs_data.pb.cc.o.d"
+  "CMakeFiles/GCS.dir/src/imgui.cpp.o"
+  "CMakeFiles/GCS.dir/src/imgui.cpp.o.d"
+  "CMakeFiles/GCS.dir/src/imgui_demo.cpp.o"
+  "CMakeFiles/GCS.dir/src/imgui_demo.cpp.o.d"
+  "CMakeFiles/GCS.dir/src/imgui_draw.cpp.o"
+  "CMakeFiles/GCS.dir/src/imgui_draw.cpp.o.d"
+  "CMakeFiles/GCS.dir/src/imgui_impl_glfw.cpp.o"
+  "CMakeFiles/GCS.dir/src/imgui_impl_glfw.cpp.o.d"
+  "CMakeFiles/GCS.dir/src/imgui_impl_opengl3.cpp.o"
+  "CMakeFiles/GCS.dir/src/imgui_impl_opengl3.cpp.o.d"
+  "CMakeFiles/GCS.dir/src/imgui_tables.cpp.o"
+  "CMakeFiles/GCS.dir/src/imgui_tables.cpp.o.d"
+  "CMakeFiles/GCS.dir/src/imgui_widgets.cpp.o"
+  "CMakeFiles/GCS.dir/src/imgui_widgets.cpp.o.d"
+  "CMakeFiles/GCS.dir/src/main.cpp.o"
+  "CMakeFiles/GCS.dir/src/main.cpp.o.d"
+  "GCS"
+  "GCS.pdb"
+  "drone_data.pb.cc"
+  "drone_data.pb.h"
+  "gcs_data.pb.cc"
+  "gcs_data.pb.h"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GCS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
