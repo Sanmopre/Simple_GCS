56f994552e0d5cbdbfeb134b9a51bd63464c863a
Santiago Moliner <40120264+Sanmopre@users.noreply.github.com>
2024-03-07 21:41:10 +0100

    Dummy servo motor code

--- test/recive_tester.py
+++ test/recive_tester.py
@@ -1,6 +1,9 @@
 import socket
 import gcs_data_pb2
 from google.protobuf.json_format import MessageToDict
+from gpiozero import AngularServo
+
+servo = AngularServo(18, min_pulse_width=0.0006, max_pulse_width=0.0023)
 
 def receive_udp_messages(address, port):
     # Create a UDP socket
@@ -21,9 +24,12 @@ def receive_udp_messages(address, port):
             gcs_data_dict = MessageToDict(gcs_data)
             print("Received message from GCS:", gcs_data_dict)
 
+            if 'pitch' in gcs_data_dict:
+                servo.angle = gcs_data_dict['pitch']
+
     except KeyboardInterrupt:
         # Close the socket when the script is interrupted
         sock.close()
 
 # Use the function
-receive_udp_messages("192.168.1.110", 9124)
+receive_udp_messages("192.168.1.145", 9124)
